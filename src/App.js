b6b5fdf1fd990e3b3f051374fbbc41c11c4f6ee6
iHarryD <anuraagvr555@gmail.com>
2022-03-07 13:36:53 +0530

    added basic stylesheets

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+import "./css/generic.css";
+import "./css/utilities.css";
+import "./css/variables.css";
+import "./css/typography.css";
+
 function App() {
   return <div className="App"></div>;
 }
