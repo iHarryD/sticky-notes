3ab7c322c0bb27f37bdaa6f3ee381ea7a3d10e47
iHarryD <anuraagvr555@gmail.com>
2022-03-07 19:47:24 +0530

    added todo

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,14 @@
+import { Link } from "react-router-dom";
+
 import "./css/style.css";
 
 export default function Header() {
   return (
     <header>
       <nav className="navbar --horizontal-flex">
-        <div className="--bold-700">STICKY NOTES</div>
+        <Link to="/" className="--bold-700">
+          STICKY NOTES
+        </Link>
         <button className="btn --primary-btn --has-hover-overlay">login</button>
       </nav>
     </header>
---
+++ src/components/todos_container/TodosContainer.js
@@ -0,0 +1,11 @@
+import "./css/style.css";
+
+import Todo from "../todo/Todo";
+
+export default function TodosContainer() {
+  return (
+    <section className="your-todos-section">
+      <Todo />
+    </section>
+  );
+}
